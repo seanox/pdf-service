aae6642a66a1f0effe526e0e3d2d88d239e5f5ea
seanox <service@seanox.de>
2022-08-06 19:01:05 +0200

    #0000 Review: Optimizations

--- src/test/java/com/seanox/pdf/UnitTest.java
+++ src/test/java/com/seanox/pdf/UnitTest.java
@@ -50,7 +50,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 import java.util.TreeMap;
 import java.util.logging.Level;
